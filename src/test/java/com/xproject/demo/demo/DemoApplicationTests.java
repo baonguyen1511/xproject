5594bdcc54eda88505a6a52330105f34fc86bf64
Bao Nguyen Tran Xuan <bao.nguyentx@tpssoft.com>
2020-09-01 10:35:15 +0700

    First commit
    Signed-off-by: Bao Nguyen Tran Xuan <bao.nguyentx@tpssoft.com>

---
+++ src/test/java/com/xproject/demo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xproject.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
